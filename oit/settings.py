6d616d3b35500dab316ab8c83e7b3664a29c8f3f
dev <dev@example.com>
2018-04-29 14:58:55 +0545

    Added static root in project settings

--- oit/settings.py
+++ oit/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Added by developer after this
+# So that collectstatic works
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
