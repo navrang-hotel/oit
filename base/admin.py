30411984e97ec6d4bfe48a6a4233bd843511c1e9
dev <dev@example.com>
2018-05-11 00:23:51 +0545

    Started project models

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# =============================
+# Added by developer after this
+# =============================
+
+from .models import Project
+
+admin.site.register(Project)
+
--- base/models.py
+++ base/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+# =============================
+# Added by developer after this
+# =============================
+
+from django.contrib.auth.models import User
+
+
+class Project(models.Model):
+    """Model class for project."""
+
+    STATUS = (
+        ('D', 'Design',),
+        ('V', 'Develop',),
+        ('M', 'Maintainance',),
+    )
+
+    name = models.CharField(max_length=200)
+    status = models.CharField(max_length=1, choices=STATUS)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """String representation of object."""
+        
+        return self.name
+
