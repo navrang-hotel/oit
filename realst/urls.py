e1721d92a0e76df7379a4f49eabcf37bf812fb64
odev01 <odev01@example.com>
2018-07-06 17:41:34 +0545

    Update realst

--- realst/urls.py
+++ realst/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='realst-index'),
+    path('homeloans/', views.home_loan, name='realst-homeloan'),
+    path('aboutus/', views.about_us, name='realst-aboutus'),
+    path('contactus/', views.contact_us, name='realst-contactus'),
+    path('pricing/', views.pricing, name='realst-pricing'),
 ]
 
--- realst/views.py
+++ realst/views.py
@@ -14,4 +14,35 @@ def index(request):
 
     return render(request, template, context)
 
+def home_loan(request):
+    """View function for home loan page."""
+
+    template = 'realst/home_loans.html'
+    context = {}
+
+    return render(request, template, context)
+
+def about_us(request):
+    """View function for about us page."""
+
+    template = 'realst/about_us.html'
+    context = {}
+
+    return render(request, template, context)
+
+def contact_us(request):
+    """View function for contact us page."""
+
+    template = 'realst/contact_us.html'
+    context = {}
+
+    return render(request, template, context)
+
+def pricing(request):
+    """View function for pricing page."""
+
+    template = 'realst/pricing.html'
+    context = {}
+
+    return render(request, template, context)
 
